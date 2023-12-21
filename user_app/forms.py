6a1251990b78409bf233759d4e4fe9556e3e4b91
MeriemBouy <125984142+MeriemBouy@users.noreply.github.com>
2023-12-21 18:56:55 +0100

    Delete user_app directory

--- user_app/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from user_app.models import Moderator
-
-
-class ModeratorForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        model = Moderator
